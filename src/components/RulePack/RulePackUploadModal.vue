052e4add517eb35a9f3a12a37ac223958e9e8db5
Benoît Viguier <ildyria@users.noreply.github.com>
2024-06-24 15:21:15 +0200

    Version 3.3.1: fix upload modal (#42)

--- src/components/RulePack/RulePackUploadModal.vue
+++ src/components/RulePack/RulePackUploadModal.vue
@@ -49,19 +49,17 @@
         </div>
       </form>
 
-      <template #modal-footer>
+      <template #footer>
         <div class="w-100">
           <b-button
             variant="primary"
-            class="float-right"
+            class="float-end"
             v-on:click="handleOk"
-            :disabled="(file && file.length == 0) || !versionState"
+            :disabled="!file || !versionState"
           >
             UPLOAD
           </b-button>
-          <b-button variant="secondary" class="float-right mr-3" v-on:click="hide">
-            CLOSE
-          </b-button>
+          <b-button variant="secondary" class="float-end me-3" v-on:click="hide"> CLOSE </b-button>
         </div>
       </template>
     </b-modal>
@@ -74,12 +72,19 @@ import SpinnerVue from '@/components/Common/SpinnerVue.vue';
 import PushNotification from '@/utils/push-notification';
 import RulePackService from '@/services/rule-pack-service';
 import { nextTick, ref, type Ref } from 'vue';
-import type { BvEvent } from 'bootstrap-vue-next';
+import {
+  BFormFile,
+  BButton,
+  BFormGroup,
+  BFormInput,
+  BModal,
+  type BvEvent,
+} from 'bootstrap-vue-next';
 
 const loadedData = ref(true);
 const rule_pack_upload_modal = ref();
 
-const file = ref([] as File[]);
+const file = ref(undefined) as Ref<File | undefined>;
 const version = ref('');
 const versionState = ref(null) as Ref<boolean | null>;
 const emit = defineEmits(['on-file-upload-suceess']);
@@ -90,14 +95,14 @@ function show(): void {
 
 function hide(): void {
   version.value = '';
-  file.value = [];
+  file.value = undefined;
   versionState.value = null;
   rule_pack_upload_modal.value.hide();
 }
 
 function resetModal(): void {
   version.value = '';
-  file.value = [];
+  file.value = undefined;
   versionState.value = null;
 }
 
@@ -118,7 +123,7 @@ function handleOk(bvModalEvt: BvEvent | MouseEvent) {
 
 function submitForm() {
   loadedData.value = false;
-  RulePackService.uploadRulePack(version.value, file.value[0])
+  RulePackService.uploadRulePack(version.value, file.value as File)
     .then((response) => {
       emit('on-file-upload-suceess');
       if (response && response.status === 200) {
